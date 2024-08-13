92a18e04931075f87311297d2b7bb8095c6bd68f
alestroc <alestroc@gmail.com>
2024-08-13 18:29:59 +0200

    splash screen added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import WebView from 'react-native-webview';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide(); // Hides the splash screen once the app is ready
+  }, []);
+
   return (
     <SafeAreaView style={styles.main}>
       <WebView
