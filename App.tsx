37cf3a75a9465f62cff8f151e645f1d7fb17c706
mcsamat <58939139+mcsamat@users.noreply.github.com>
2024-09-16 11:08:41 +0200

    (feat): prevent app exit on back

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
-import WebView from 'react-native-webview';
+import {BackHandler, Platform, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+
 import SplashScreen from 'react-native-splash-screen';
+import WebView from 'react-native-webview';
 
 const App = () => {
+  const webviewRef = useRef(null);
+
   useEffect(() => {
     SplashScreen.hide(); // Hides the splash screen once the app is ready
+
+    if (Platform.OS === 'android') {
+      const backHandler = BackHandler.addEventListener(
+        'hardwareBackPress',
+        () => {
+          if (webviewRef.current) {
+            webviewRef.current.goBack(); // Navigate back in WebView if possible
+            return true; // Prevent default behavior (closing the app)
+          }
+          return false; // If no history to go back to, allow default behavior (closing the app)
+        },
+      );
+
+      return () => backHandler.remove(); // Clean up the event listener
+    }
   }, []);
 
   return (
     <SafeAreaView style={styles.main}>
       <WebView
+        ref={webviewRef}
         style={styles.web}
         originWhitelist={['*']}
-        source={{
-          uri: 'https://hosts.eventoapp.it/',
+        source={{uri: 'https://hosts.eventoapp.it/'}}
+        onNavigationStateChange={navState => {
+          if (webviewRef.current && !navState.canGoBack) {
+            // BackHandler.exitApp(); // Close the app if no more history to go back to
+          }
         }}
       />
     </SafeAreaView>
